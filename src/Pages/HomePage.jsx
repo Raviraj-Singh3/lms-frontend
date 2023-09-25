f3ea4d2cc46b58e1eb92681b2d67b8a243bc6fb7
Raviraj-Singh3 <iamtheraviraj@gmail.com>
2023-09-26 02:06:30 +0530

    added basic layout and integrated to homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+
 import HomeLayout from "../Layouts/HomeLayout";
+import homePageImage from './../assets/Images/homePageMainImage.png'
 
 function HomePage(){
     return(
@@ -18,6 +21,24 @@ function HomePage(){
                     We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
                     </p>
 
+                    <div className="space-x-6">
+                        <Link to="/courses">
+                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                                Explore courses
+                            </button>
+                        </Link>
+
+                        <Link to="/contact">
+                            <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                                Contact Us
+                            </button>
+                        </Link>
+                    </div>
+
+                </div>
+
+                <div className="w-1/2 flex items-center justify-center">
+                    <img src={homePageImage} alt="homepage image" />
                 </div>
 
             </div>
